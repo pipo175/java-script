907ef01da981b2617a8d36769a69b011d3ee5b06
Abanoub Hanna <abanoubhanna138@gmail.com>
2024-07-31 12:26:03 +0300

    upload project

---
+++ main.js
@@ -0,0 +1,9 @@
+document.getElementById('darkModeBtn').addEventListener('click', () => {
+    document.body.classList.add('dark-mode');
+});
+
+document.getElementById('lightModeBtn').addEventListener('click', () => {
+    document.body.classList.remove('dark-mode');
+});
+
+
